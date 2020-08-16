01d10e69c723a8a2476ac0a0e9d593d7bf7a642c
unknown <gkrajkiran@gmail.com>
2020-08-16 17:28:19 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A dashboard to see the spread of corona virus across different countries. Which countries did well, which countries did not. ',
+    author='Rajkiran Katenahalli',
+    license='MIT',
+)
